65edcdc6285ede70221032580d7617b0261e39aa
xksqs <erlichsam9@gmail.com>
2026-04-17 13:05:28 +1000

    fpost

---
+++ pw-sec/main.js
@@ -0,0 +1,22 @@
+const submit = document.getElementById("submit")
+const form = document.getElementById("info")
+
+function fpost(url, name, pw) {
+    fetch(url, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        username: name,
+        password: pw
+    })
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
+}
+
+submit.addEventListener("click", () => {
+    fpost("https://xksqs-website.pages.dev/api", form.elements["username"], form.elements["password"])
+});
