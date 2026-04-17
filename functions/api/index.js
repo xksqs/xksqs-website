63fac31ebe7b0ce45e4e68cfa51bebb2ec3a5d31
xksqs <erlichsam9@gmail.com>
2026-04-17 13:59:07 +1000

    api fix v2

--- functions/api/index.js
+++ functions/api/index.js
@@ -1,43 +1,44 @@
-/*
-
-Name: Serverside Password Manager (JS)
-Author: xksqs
-Version: 0.1
-
-*/
-
 const bcrypt = require("bcryptjs");
 
-
 export async function onRequestPost({ request }) {
-    const data = await request.json();
+    try {
+        const data = await request.json();
 
-    return new Response(
-        JSON.stringify({
-            received: await hashPassword(data.password),
-            status: "OK"
-        }),
-        {
-            headers: {
-            "Content-Type": "application/json"
-            }
+        if (!data.password) {
+            return new Response(JSON.stringify({ error: "Missing password" }), {
+                status: 400,
+                headers: { "Content-Type": "application/json" }
+            });
         }
-    );
+
+        const hash = await hashPassword(data.password);
+
+        return new Response(JSON.stringify({
+            received: hash,
+            status: "OK"
+        }), {
+            headers: { "Content-Type": "application/json" }
+        });
+
+    } catch (err) {
+        return new Response(JSON.stringify({
+            error: "Invalid JSON or server error"
+        }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" }
+        });
+    }
 }
 
 export async function onRequestGet() {
-    return new Response("Send a POST request to use this API", {
-        status: 200,
-        headers: {
-            "Content-Type": "text/plain"
-        }
+    return new Response(JSON.stringify({
+        message: "Send a POST request"
+    }), {
+        headers: { "Content-Type": "application/json" }
     });
 }
 
 async function hashPassword(password) {
-    const saltRounds = 10;
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hash = await bcrypt.hash(password, salt);
-    return hash;
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
 }
- 
